98f375f468d5059ef29f0bfcc95ddec15edf7416
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-03-17 09:26:47 +0200

    do magic

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
-export const fetchImagesByName = async (pageNum = 1, searchQuery = '') => {
+export const fetchImagesByName = async (searchQuery = '', pageNum = 1 ) => {
   const BASE_URL = 'https://pixabay.com/api/';
-  const params = {
-    key: '11497081-cc65ac3ee2055db377a22cee7',
-    q: searchQuery,
-    image_type: 'photo',
-    page: pageNum,
-    orientation: 'horizontal',
-    per_page: 12,
-  };
-  const response = await axios.get(BASE_URL, { params });
+  const API_KEY = '11497081-cc65ac3ee2055db377a22cee7';
+  // const params = {
+  //   key: '11497081-cc65ac3ee2055db377a22cee7',
+  //   q: searchQuery,
+  //   image_type: 'photo',
+  //   page: pageNum,
+  //   orientation: 'horizontal',
+  //   per_page: 12,
+  // };
+  const response = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q="${searchQuery}"&image_type=photo&orientation=horizontal&per_page=12&page=${pageNum}`
+  );
   console.log(response.data)
   return response.data;
 };
